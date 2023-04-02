6d39e01d2b5fb7cea36717c2c9451f5481ee66a7
ANDRIASATARINTSOA Manohisoa <106870328+Man0212@users.noreply.github.com>
2023-04-02 21:56:11 +0300

    Add files via upload

--- source/FrontServlet.java
+++ source/FrontServlet.java
@@ -1,16 +1,60 @@
 package etu1946.framework.servlet;
 
 import etu1946.framework.Mapping;
+import etu1946.framework.annotation.Url;
+
 import java.io.*;
 import javax.servlet.*;
 import javax.servlet.http.*;
 import java.util.HashMap;
 import java.util.Map;
+import java.lang.annotation.Target;
+import java.lang.annotation.Retention;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Annotation;
+import java.lang.reflect.Method;
+
 
 public class FrontServlet extends HttpServlet {
 
     HashMap<String, Mapping> mappingUrls;
 
+    public void init() throws ServletException {
+        super.init();
+        this.setMappingUrls(analyzeModelsDirectory());
+    }
+    
+    private HashMap<String, Mapping> analyzeModelsDirectory() {
+        HashMap<String, Mapping> mappings = new HashMap<>();
+        String MODELS_DIR = getServletContext().getRealPath("/WEB-INF/classes");
+
+        File dir = new File(MODELS_DIR);
+        File[] files = dir.listFiles();
+
+        for (File file : files) {
+            if (file.isFile()) {
+                try {
+                    Class<?> cls = Class.forName("etu1946.framework.models."+getClassNameFromFile(file));
+                    Method[] methods = cls.getDeclaredMethods();
+                    for (Method method : methods) {
+                            String annotationName = method.getAnnotation(Url.class).value();
+                            Mapping mapping = new Mapping(getClassNameFromFile(file), method.getName());
+                            mappings.put(annotationName, mapping);
+                    }
+                } catch (ClassNotFoundException e) {
+                   
+                }
+            }
+        }
+        return mappings;
+    }
+
+    private String getClassNameFromFile(File file) {
+        String fileName = file.getName();
+        return fileName.substring(0, fileName.lastIndexOf("."));
+    }
+    
     public void setMappingUrls(HashMap<String, Mapping> mappingUrls) {
         this.mappingUrls = mappingUrls;
     }
@@ -31,6 +75,4 @@ public class FrontServlet extends HttpServlet {
         PrintWriter out = resp.getWriter();
         out.println("");
     }
-
-
 }
