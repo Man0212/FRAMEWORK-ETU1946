d647bbccf312c234e00138167bd1f950f0711b8a
ANDRIASATARINTSOA Manohisoa <106870328+Man0212@users.noreply.github.com>
2023-04-02 22:02:03 +0300

    Add files via upload

--- TEST-FRAMEWORK/src/java/etu1946/framework/models/Emp.java
+++ TEST-FRAMEWORK/src/java/etu1946/framework/models/Emp.java
@@ -1,12 +1,14 @@
 package etu1946.framework.models;
 import etu1946.framework.annotation.Url;
+import etu1946.framework.view.ModelView;
+
 
 public class Emp {
 
-    String name;
-
-    @Url("add-emp")
-    public void insert() {
-        System.out.println("insertion");
+    @Url("hello")
+    public ModelView welcome() {
+        ModelView vue = new ModelView();
+        vue.setView("list.html");
+        return vue;
     }
 }
