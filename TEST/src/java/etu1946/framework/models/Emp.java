5313eeb8662d9da70f09e2a249eb35a21248231f
ANDRIASATARINTSOA Manohisoa <106870328+Man0212@users.noreply.github.com>
2023-07-04 10:34:29 +0200

    Add files via upload

--- TEST/src/java/etu1946/framework/models/Emp.java
+++ TEST/src/java/etu1946/framework/models/Emp.java
@@ -1,17 +1,34 @@
 package etu1946.framework.models;
 import etu1946.framework.annotation.Url;
+import etu1946.framework.annotation.Scope;
 import etu1946.framework.view.ModelView;
 import jakarta.servlet.http.HttpServletRequest;
 
-
+@Scope("singleton")
 public class Emp {
+  String Nom;
+Integer Age;
+
+public String getNom() {
+    return Nom;
+}
+public void setNom(String nom) {
+    Nom = nom;
+}
+public Integer getAge() {
+    return Age;
+}
+public void setAge(Integer age) {
+    Age = age;
+}
 
     @Url(value="hello",params = {"age","nom"})
     public ModelView welcome(int age,String nom) {
         ModelView vue = new ModelView();
         try{
         vue.setView("list.jsp");
-        vue.addItem("test",age*2);
+        vue.addItem("test",age+nom);
+        
          }catch(Exception e){
                 
          }
